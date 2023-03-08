ec3dda2816630eed5dc8bc5364a4f2bf10d9faa6
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-03-07 16:20:37 -0800

    added bootstrap and css

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+{
+    "Questions": [
+        {
+            "Quiz": "Q.1) What does HTML stand for?",
+            "option1": "Hyper Type Markup Language",
+            "option2": "Hyper Link and Text Markup Language",
+            "option3": "Hyper Text Markup Language",
+            "answer": "Hyper Text Markup Language"
+},
+        {
+            "Quiz": "Q.2) The condition in an if / else statement is enclosed with ______."
+            "option1": "Quotes",
+            "option2": "Curly Brackets"
+            "option3": "Parenthesis"
+            "answer": "Parenthesis"
+}
+]}
