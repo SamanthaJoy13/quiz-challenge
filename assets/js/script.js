430dedee308015b9490d0447a3a2e81f925bd116
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-06-02 13:59:03 -0700

    updated timer div in html, and updated js timer function, added the questions and answers in js, and attempted timer css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,113 @@
 function startQuiz() {
-const timerEl = document.getElementById('timer');
-  const questionCon = document.getElementById('questionsContainer');
-  const questionEl = document.getElementById('question');
-  const answersCon = document.getElementById('answers');
-
-
-  const startButton = document.getElementById('start');
-  startButton.addEventListener('click', () => {
-    let currentQuestionIndex = 0;
-    let time = 75;
-    let timerInterval;
-
-    startTimer();
-
-    function startTimer() {
-      timerInterval = setInterval(updateTimer, 1000);
-    }
-
-    function updateTimer() {
-      time--;
-      timerContainer.textContent = `Time: ${time}`;
-
-      if (time <= 0) {
-        // Time's up, end the game
-        endGame();
+    const timerEl = document.getElementById('timerCon');
+    const questionEl = document.getElementById('question');
+    const answersContainer = document.getElementById('answers');
+  
+    const startButton = document.querySelector('.start');
+    startButton.addEventListener('click', () => {
+      let currentQuestionIndex = 0;
+      let time = 75;
+      let timerInterval;
+  
+      startTimer();
+  
+      function startTimer() {
+        timerInterval = setInterval(updateTimer, 1000);
       }
-    }
-
-    const quizQuestions = [
-        {
-            question: 'Commonly used data types DO NOT include:',
-            answers: ['strings', 'booleans', 'alerts', 'numbers'],
-            correctAnswer: 'alerts'
-        },
-        {
-            
-
-
-
-      // Clear previous answer buttons
-      answersContainer.innerHTML = '';
-
-      // Create answer buttons for each possible answer
-      for (let i = 0; i < currentQuestion.answers.length; i++) {
-        const answer = currentQuestion.answers[i];
-
-        const answerButton = document.createElement('button');
-        answerButton.textContent = answer;
-        answerButton.addEventListener('click', () => {
-          // Handle answer selection
-          handleAnswer(answer);
-        });
-
-        answersContainer.appendChild(answerButton);
-      }
-    }
-
-    function handleAnswer(selectedAnswer) {
-      const currentQuestion = quizQuestions[currentQuestionIndex];
-
-      // Compare selected answer with the correct answer
-      if (selectedAnswer === currentQuestion.correctAnswer) {
-        // Answer is correct
-        // ...
-      } else {
-        // Answer is incorrect, subtract 15 seconds
-        time -= 10;
-
-        // Check if the timer goes below 0
+  
+      function updateTimer() {
+        time--;
+        timerEl.textContent = `Time: ${time}`;
+  
         if (time <= 0) {
           // Time's up, end the game
           endGame();
-          return;
         }
       }
-
-      // Move to the next question
-      currentQuestionIndex++;
-      if (currentQuestionIndex < quizQuestions.length) {
-        displayQuestion();
-      } else {
-        // End the game when all questions are answered
-        endGame();
+  
+      const quizQuestions = [
+        {
+          question: 'Commonly used data types DO NOT include:',
+          answers: ['1. Strings', '2. Booleans', '3. Alerts', '4. Numbers'],
+          correctAnswer: '3. Alerts'
+        },
+        {
+          question: 'String values must be enclosed within ______ when being assigned to variables.',
+          answers: ['1. Commas', '2. Curly brackets', '3. Quotes', '4. Parentheses'],
+          correctAnswer: '3. Quotes'
+        },
+        {
+          question: 'The else/if statement is enclosed with the following:',
+          answers: ['1. Quotes', '2. Curly brackets', '3. Parentheses', '4. Square brackets'],
+          correctAnswer: '2. Curly brackets'
+        },
+      ];
+  
+      function displayQuestion() {
+        const currentQuestion = quizQuestions[currentQuestionIndex];
+        questionEl.textContent = currentQuestion.question;
+  
+        // Clear previous answer buttons
+        answersContainer.innerHTML = '';
+  
+        // Create answer buttons for each possible answer
+        for (let i = 0; i < currentQuestion.answers.length; i++) {
+          const answer = currentQuestion.answers[i];
+  
+          const answerButton = document.createElement('button');
+          answerButton.textContent = answer;
+          answerButton.addEventListener('click', () => {
+            // Handle answer selection
+            handleAnswer(answer);
+          });
+  
+          answersContainer.appendChild(answerButton);
+        }
       }
-    }
-
-    function endGame() {
-      clearInterval(timerInterval);
-
-      // Hide question container
-      questionContainer.style.display = 'none';
-
-      // Show the form to save initials and score
-      // ...
-    }
-
-    // Display the first question
-    displayQuestion();
-  });
-}
-
-document.addEventListener('DOMContentLoaded', startQuiz);
+  
+      function handleAnswer(selectedAnswer) {
+        const currentQuestion = quizQuestions[currentQuestionIndex];
+  
+        // Compare selected answer with the correct answer
+        if (selectedAnswer === currentQuestion.correctAnswer) {
+          // Answer is correct
+          // ...
+        } else {
+          // Answer is incorrect, subtract 10 seconds
+          time -= 10;
+  
+          // Check if the timer goes below 0
+          if (time <= 0) {
+            // Time's up, end the game
+            endGame();
+            return;
+          }
+        }
+  
+        // Move to the next question
+        currentQuestionIndex++;
+        if (currentQuestionIndex < quizQuestions.length) {
+          displayQuestion();
+        } else {
+          // End the game when all questions are answered
+          endGame();
+        }
+      }
+  
+      function endGame() {
+        clearInterval(timerInterval);
+  
+        // Hide question container
+        document.getElementById('questionsContainer').style.display = 'none';
+  
+        // Show the form to save initials and score
+        // ...
+      }
+  
+      // Display the first question
+      displayQuestion();
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', startQuiz);
+  
